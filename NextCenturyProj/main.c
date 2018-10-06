3d329553fd33356cf668e6044dc91f7e861e0695
Adam Whetton <Adam.Whetton@opensynergy.com>
2018-10-05 20:34:39 -0600

    first commit

---
+++ NextCenturyProj/main.c
@@ -0,0 +1,21 @@
+/*
+ * NextCenturyProj.c
+ *
+ * Created: 10/5/2018 8:31:20 PM
+ * Author : awh
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
